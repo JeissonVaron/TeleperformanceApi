8da79ebc9d8e8ed0c4e68b277e5540c3134331a4
Jeisson Camilo Rodriguez Varon <jeisson.rodriguezv@chevyplan.com.co>
2021-06-01 22:56:58 -0500

    merge

--- TeleperformanceApi/obj/Release/netcoreapp3.1/TeleperformanceApi.AssemblyInfo.cs
+++ TeleperformanceApi/obj/Release/netcoreapp3.1/TeleperformanceApi.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("35086308-df42-47fb-baf7-086288c628a1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeleperformanceApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
